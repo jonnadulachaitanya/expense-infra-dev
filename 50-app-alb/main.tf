0062cd8a40ba030337635e7f51cc31da6cb7f6c0
chaitanyajonnadula <chaitanyajonnadula98@gmail.com>
2025-08-17 20:21:35 +0530

    committed

---
+++ 50-app-alb/main.tf
@@ -0,0 +1,55 @@
+module "app_alb" {
+  source = "terraform-aws-modules/alb/aws"
+
+  name    = "${local.resource_name}-app-alb"
+  vpc_id  = local.vpc_id
+  subnets = local.private_subnet_ids
+  internal = true #So no public access
+  security_groups = [data.aws_ssm_parameter.app_alb_sg_id.value]
+  create_security_group = false
+
+  
+
+  tags = merge (
+    var.common_tags,
+    var.app_alb_tags
+  )
+
+}
+
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = module.app_alb.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type = "fixed-response"
+
+    fixed_response {
+      content_type = "text/html"
+      message_body = "<h1>Hello, I am from Application ALB <h1>"
+      status_code  = "200"
+    }
+  }
+}
+
+
+module "records" {
+  source  = "terraform-aws-modules/route53/aws//modules/records"
+
+  zone_name = var.zone_name
+
+  records = [
+    {
+      name    = "*.app-alb-${var.environment}" #*.app-dev.chaitanyaproject.online
+      type    = "A"
+      alias   = {
+        name    = module.app_alb.dns_name
+        zone_id = module.app_alb.zone_id
+      }
+
+    allow_overwrite = true
+    }
+  ]
+}
---
+++ 50-app-alb/variables.tf
@@ -0,0 +1,25 @@
+variable "project_name" {
+    default = "expense"
+}
+
+variable "environment" {
+    default = "development"
+}
+
+variable "common_tags" {
+    default = {
+        Project = "expense"
+        terraform = "true"
+        Environment = "dev"
+    }
+}
+
+variable "zone_name" {
+    default = "chaitanyaproject.online"
+}
+
+variable "app_alb_tags" {
+    default = {
+        component = "app-alb"
+    }
+}
