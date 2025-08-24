9e0c6c9fa0b0c09bc0132f09fd8e29e23059cce3
chaitanyajonnadula <chaitanyajonnadula98@gmail.com>
2025-08-24 21:56:02 +0530

    committed

--- 60-backend/backend.sh
+++ 60-backend/backend.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-component = $1
-environment = $2
+component=$1
+environment=$2
 
 echo "component: $component, environment: $2"
 ansible-pull -i localhost, -u https://github.com/jonnadulachaitanya/expense-ansible-roles-tf main.yaml -e component=$component -e environment=$environment
