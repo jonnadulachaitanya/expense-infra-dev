24ed30222558e86356bc0427628ddd75b3f317a3
chaitanyajonnadula <chaitanyajonnadula98@gmail.com>
2025-08-26 21:43:47 +0530

    file committed

---
+++ 50-app-alb/parameters.tf
@@ -0,0 +1,6 @@
+
+resource "aws_ssm_parameter" "aws_lb_listener" {
+  name  = "/${var.project_name}/${var.environment}/aws_lb_listener"
+  type  = "StringList"
+  value = aws_lb_listener.http.arn
+}
