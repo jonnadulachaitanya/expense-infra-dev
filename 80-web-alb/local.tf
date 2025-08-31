615018e331d5088a46a66ea16df2e455df297275
chaitanyajonnadula <chaitanyajonnadula98@gmail.com>
2025-08-31 16:19:57 +0530

    committed

--- 80-web-alb/local.tf
+++ 80-web-alb/local.tf
@@ -3,5 +3,5 @@ locals {
   vpc_id                = data.aws_ssm_parameter.vpc_id.value
   public_subnet_ids     = split(",", data.aws_ssm_parameter.public_subnet_ids.value)
   https_certificate_arn = data.aws_ssm_parameter.https_certificate_arn.value
-  web_alb_sg_id         = data.aws_ssm_parameter.web_alb_sg_id
+  web_alb_sg_id         = data.aws_ssm_parameter.web_alb_sg_id.value
 }
