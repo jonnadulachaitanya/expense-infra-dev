9e5a1d682ea9bf6d07c7b19cafd7d1fd9c995ec5
chaitanyajonnadula <chaitanyajonnadula98@gmail.com>
2025-09-01 19:45:45 +0530

    file committed

--- 90-frontend/frontend.sh
+++ 90-frontend/frontend.sh
@@ -4,5 +4,5 @@ component=$1
 environment=$2
 
 echo "component: $component", and environment: $environment
-dnf install ansible -y
+sudo dnf install ansible -y
 ansible-pull -i localhost, -U https://github.com/jonnadulachaitanya/expense-ansible-roles-tf.git main.yaml -e component=$component -e environment=$environment
